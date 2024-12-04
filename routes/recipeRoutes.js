797deb9124c713596283037f1f99214049c744da
Sirus Salari <salaris@oregonstate.edu>
2024-12-03 20:49:13 -0800

    Setup route to exclude recipe

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -1,7 +1,9 @@
 const { getRecipes } = require('../controllers/recipeController');
+const { excludeRecipe } = require('../controllers/recipeController');
 const express = require('express');
 const router = express.Router();
 
 router.get('/recipes/:userId', getRecipes);
+router.post('/exclude-recipe', excludeRecipe);
 
 module.exports = router;
