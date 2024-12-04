945244cf76b438f29ca266cd80cb5683d16f236f
Sirus Salari <salaris@oregonstate.edu>
2024-12-03 19:54:35 -0800

    Use recipe route in index.js

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const recipeRoutes = require('./routes/recipeRoutes');
 
 const app = express();
 
 dotenv.config();
 app.use(cors());
 app.use(express.json());
+
+app.use('/recipes', recipeRoutes);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+});
